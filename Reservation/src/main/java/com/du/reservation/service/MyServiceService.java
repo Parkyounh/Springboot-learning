19475227b063c5aa8b3e1bb6d09ce959bee9c760
Parkyounh <dkrfud9@naver.com>
2025-09-19 18:42:22 +0900

    Reservation 예약 시스템

---
+++ Reservation/src/main/java/com/du/reservation/service/MyServiceService.java
@@ -0,0 +1,30 @@
+package com.du.reservation.service;
+
+
+import com.du.reservation.dao.ServiceMapper;
+import com.du.reservation.domain.MyService;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class MyServiceService {
+    @Autowired
+    private ServiceMapper serviceMapper;
+
+    public List<MyService> getAllServices() {
+        return serviceMapper.findAll();
+    }
+
+    public void addService(MyService service) {
+        serviceMapper.insert(service);
+    }
+
+    public void deleteService(Long id) {
+        serviceMapper.delete(id);
+    }
+}
+
