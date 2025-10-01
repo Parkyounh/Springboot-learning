b92ae7ea07b1144d986f9d515738f2cab7a1194a
Parkyounh <dkrfud9@naver.com>
2025-10-01 19:03:47 +0900

    이미지 path db에 저장, 띄우기

--- 20250930Em/src/main/java/com/du/em0930/config/WebConfig.java
+++ 20250930Em/src/main/java/com/du/em0930/config/WebConfig.java
@@ -22,8 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file:"+uploadPath+"/");
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + uploadPath + "/");
     }
 }
