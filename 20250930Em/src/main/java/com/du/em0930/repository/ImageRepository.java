efa42592e143a9c17697229a985a7d4b0b59e12f
Parkyounh <dkrfud9@naver.com>
2025-10-14 19:31:59 +0900

    Revert "파이썬 코드"
    This reverts commit 60ab5b098c389ee9a56d00855cee19102240cd53.

--- 20250930Em/src/main/java/com/du/em0930/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.du.em0930.repository;
-
-import com.du.em0930.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
-
